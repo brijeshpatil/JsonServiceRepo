96279ee47773d2a4de807e59e87073bc33b109b5
brijeshpatil <brijesh.patil@tops-int.com>
2016-08-09 03:43:52 +0530

    Uploaded Project Files

---
+++ Controllers/MyCRUDController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JsonServiceExample.Controllers
+{
+    public class MyCRUDController : ApiController
+    {
+
+        JsonServiceExampleDBEntities db = new JsonServiceExampleDBEntities();
+
+        [HttpGet]
+        public List<userinfo> GetAllUsers()
+        {
+            return db.userinfoes.ToList();
+        }
+
+        [HttpGet]
+        public string GetUser(string fname, string lname)
+        {
+            userinfo u = new userinfo();
+            u.fname = fname;
+            u.lname = lname;
+            db.userinfoes.Add(u);
+            db.SaveChanges();
+            return "Record Saved";
+        }
+
+    }
+}
